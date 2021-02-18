da5302f5b46c00ac84c729a964c19e6826673084
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-02-18 17:04:26 +0100

    Refactor page loading stages
    Including fixes of small issues

--- js/script.js
+++ js/script.js
@@ -5,19 +5,34 @@
  * See /LICENSE for more information.
  */
 
-const intro = document.getElementById("intro");
-const progressBar = document.getElementById("progress-bar");
 let appRedirect;
 
+// Fetching JSON
+const getJSON = async (url) => {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            const message = `An error has occured: ${response.status}`;
+            throw new Error(message);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
 // Timer & Progress Bar
-window.onload = () => {
+const timerAndProgressBar = () => {
     let start = true;
     setTimeout(() => {
         if (start) {
             start = false;
             let duration = 25;
             let progressWidth = 100;
-            const bar = document.getElementById("progress-bar");
+            const progressBar = document.getElementById("progress-bar");
             const timeout = document.getElementById("timeout");
 
             timeout.textContent = ` ${duration} sec.`;
@@ -33,87 +48,99 @@ window.onload = () => {
                     redirectTo(appRedirect);
                 } else {
                     progressWidth = progressWidth - step;
-                    bar.style.width = progressWidth + "%";
+                    progressBar.style.width = progressWidth + "%";
                 }
             }
         }
     }, 100);
 };
 
+// Rediret to URL
 const redirectTo = (url) => {
     window.location.assign(url);
 };
 
-// Fetching JSON
-const getJSON = async (url) => {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) throw new Error(response.statusText);
+// Render apps on page load
+window.addEventListener("load", () => {
+    const intro = document.getElementById("intro");
 
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        return error;
-    }
-};
+    getJSON("js/apps.json")
+        .then((data) => {
+            // Remove spinner
+            intro.innerHTML = "";
 
-getJSON("js/apps.json")
-    .then((data) => {
-        // Language Switch
-        const lang = data.langs;
-        const userLang = navigator.language || navigator.userLanguage;
-        var pageTitle;
-        var pageCountdown;
-
-        switch (userLang) {
-            case "cs":
-                pageTitle = lang.cs.title;
-                pageCountdown = lang.cs.countdown;
-                break;
-            default:
-                pageTitle = lang.en.title;
-                pageCountdown = lang.en.countdown;
-                break;
-        }
-        intro.insertAdjacentHTML(
-            "beforeBegin",
-            `<h1>${pageTitle}</h1>
-            <p class="lead text-muted">${pageCountdown}<span id="timeout"></span></p>`
-        );
-
-        // Handle special case with only one app
-        if (data.apps.length == 1) {
-            redirectTo(data.apps[0].url);
-            return;
-        }
-
-        // Rendering apps
-        for (let i = 0; i < data.apps.length; i++) {
-            const app = data.apps[i];
-            const { title, icon, id, url } = app;
-            const { selected } = data;
-            const container = document.querySelector(".container .row");
-            var appDescription = app.description;
+            // Language Switch
+            const lang = data.langs;
+            const userLang = navigator.language || navigator.userLanguage;
+            var pageTitle;
+            var pageCountdown;
 
             switch (userLang) {
                 case "cs":
-                    appDescription = app.description.cs;
+                    pageTitle = lang.cs.title;
+                    pageCountdown = lang.cs.countdown;
                     break;
                 default:
-                    appDescription = app.description.en;
+                    pageTitle = lang.en.title;
+                    pageCountdown = lang.en.countdown;
                     break;
             }
 
-            // Fallback if description is not translated
-            if (!appDescription) {
-                appDescription = app.description.en;
+            // Insert intro & countdown
+            intro.insertAdjacentHTML(
+                "beforeBegin",
+                `<h1>${pageTitle}</h1>
+            <p class="lead text-muted">${pageCountdown}<span id="timeout"> 25 sec.</span></p>`
+            );
+
+            // Insert progressbar
+            intro.innerHTML += `
+        <div class="progress">
+            <div
+                class="progress-bar progress-bar-striped progress-bar-animated"
+                id="progress-bar"
+                role="progressbar"
+                aria-valuenow="100"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                style="animation-direction: reverse; width: 100%"
+            >
+            </div>
+        </div>`;
+
+            // Handle special case with only one app
+            if (data.apps.length == 1) {
+                redirectTo(data.apps[0].url);
+                return;
             }
 
-            selected == id
-                ? (selectedApp = true) && (appRedirect = url)
-                : (selectedApp = false);
+            // Rendering apps
+            for (let i = 0; i < data.apps.length; i++) {
+                const app = data.apps[i];
+                const { title, icon, id, url } = app;
+                const { selected } = data;
+                const container = document.querySelector(".container .row");
+                var appDescription = app.description;
 
-            container.innerHTML += `
+                switch (userLang) {
+                    case "cs":
+                        appDescription = app.description.cs;
+                        break;
+                    default:
+                        appDescription = app.description.en;
+                        break;
+                }
+
+                // Fallback if description is not translated
+                if (!appDescription) {
+                    appDescription = app.description.en;
+                }
+
+                selected == id
+                    ? (selectedApp = true) && (appRedirect = url)
+                    : (selectedApp = false);
+
+                container.innerHTML += `
             <div class="col mb-5">
                 <div class="card h-100 shadow-sm mr-auto ml-auto">
                     <img class="bd-placeholder-img card-img-top p-5 border-bottom" src="${icon}" alt="${title}" />
@@ -137,8 +164,11 @@ getJSON("js/apps.json")
                     </div>
                 </div>
             </div>`;
-        }
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+            }
+            // Start timer and progress bar after apps render
+            timerAndProgressBar();
+        })
+        .catch((error) => {
+            console.error(error.message);
+        });
+});
