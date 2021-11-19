ca945072d51057532fc555b722f09df832711ec6
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-11-19 17:05:53 +0100

    Improve darkmode toggle and language switcher

--- src/components/Header/LangSwitcher.js
+++ src/components/Header/LangSwitcher.js
@@ -6,7 +6,6 @@
  */
 
 import { BoxArrowUpRight } from "react-bootstrap-icons";
-import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { t } from "ttag";
 
@@ -14,25 +13,23 @@ import LanguageMenuItem from "./LanguageMenuItem";
 
 const LangSwitcher = () => {
     return (
-        <Nav>
-            <NavDropdown
-                align="end"
-                id="nav-dropdown-languages"
-                title={t`Language`}
+        <NavDropdown
+            align="end"
+            id="nav-dropdown-languages"
+            title={t`Language`}
+        >
+            <LanguageMenuItem />
+            <NavDropdown.Divider />
+            <NavDropdown.Item
+                target="_blank"
+                href="https://docs.turris.cz/geek/contributing/translation/"
             >
-                <LanguageMenuItem />
-                <NavDropdown.Divider />
-                <NavDropdown.Item
-                    target="_blank"
-                    href="https://docs.turris.cz/geek/contributing/translation/"
-                >
-                    {t`Participate`}
-                    <sup>
-                        <BoxArrowUpRight className="ms-1" size={12} />
-                    </sup>
-                </NavDropdown.Item>
-            </NavDropdown>
-        </Nav>
+                {t`Participate`}
+                <sup>
+                    <BoxArrowUpRight className="ms-1" size={12} />
+                </sup>
+            </NavDropdown.Item>
+        </NavDropdown>
     );
 };
 
