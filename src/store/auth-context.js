f590c6a77fccd5c3ec9042d154376289a3481548
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-11-19 15:08:48 +0100

    Add API call to retrieve state in auth-context

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,6 +1,11 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 
 import PropTypes from "prop-types";
+import Spinner from "react-bootstrap/Spinner";
+import { t } from "ttag";
+
+import useHttp from "../hooks/use-http";
+import { getAuthData } from "../lib/api";
 
 const AuthContext = React.createContext({
     isLoggedIn: false,
@@ -9,10 +14,55 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
+    const [userIsLoggedIn, setUserIsLoggedIn] = useState(null);
+    const {
+        sendRequest,
+        status,
+        data: loadedData,
+        error,
+    } = useHttp(getAuthData, true);
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
+
+    useEffect(() => {
+        if (status !== "pending" && loadedData.accesstime) {
+            setUserIsLoggedIn(true);
+        } else {
+            setUserIsLoggedIn(false);
+        }
+    }, [loadedData]);
+
+    if (status === "pending") {
+        return (
+            <div id="backdrop">
+                <div className="spinner-center">
+                    <Spinner animation="border" role="status" variant="primary">
+                        <span className="visually-hidden">{t`Loading...`}</span>
+                    </Spinner>
+                </div>
+            </div>
+        );
+    }
+
+    if (error) {
+        return <p className="py-5 text-center">{error}</p>;
+    }
+
+    const loginHandler = () => {
+        window.location.assign("/login");
+    };
+
+    const logoutHandler = () => {
+        setUserIsLoggedIn(false);
+        window.location.assign("/logout");
+    };
+
     const contextValue = {
-        isLoggedIn: false,
-        login: () => {},
-        logout: () => {},
+        isLoggedIn: userIsLoggedIn,
+        login: loginHandler,
+        logout: logoutHandler,
     };
 
     return (
