a94adae0902bce47e58fa4e2de83697e2ff3230b
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-09-04 13:15:46 +0200

    Refactor ErrorBoundary component for improved error handling and UI display

--- src/utils/ErrorBoundary.jsx
+++ src/utils/ErrorBoundary.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ * Copyright (c) 2021-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
@@ -33,23 +33,21 @@ export default class ErrorBoundary extends Component {
             document.title = t`An Error Occurred - Available Applications | Turris`;
             return (
                 <div className="container">
-                    <div className="p-3 p-sm-5 border rounded-3 my-5 shadow-sm">
-                        <div className="container-fluid p-3 py-sm-5">
-                            <h1>{t`Something went wrong:`}</h1>
-                            <p className="mb-3">
-                                <code className="col-md-8 fs-4">
-                                    {error.toString()}
-                                </code>
-                            </p>
-                            <p>
-                                {t`More detailed information is available in the console of your web browser - on most browsers accessible after pressing <kbd>Ctrl+Shift+J</kbd> or <kbd>F12</kbd>.`}
-                            </p>
-                            <p
-                                dangerouslySetInnerHTML={{
-                                    __html: t`Please report this error to our support team via e-mail: <a href="mailto:tech.support@turris.cz">tech.support@turris.cz</a>.`,
-                                }}
-                            />
-                        </div>
+                    <div className="container-fluid p-3 py-sm-5">
+                        <h1>{t`Oops! Something went wrong:`}</h1>
+                        <p className="p-3 my-3 bg-secondary-subtle">
+                            <code>{error.toString()}</code>
+                        </p>
+                        <p
+                            dangerouslySetInnerHTML={{
+                                __html: t`More detailed information is available in the console of your web browser. On most browsers accessible after pressing <kbd><kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>J</kbd></kbd> or <kbd>F12</kbd>.`,
+                            }}
+                        ></p>
+                        <p
+                            dangerouslySetInnerHTML={{
+                                __html: t`Please report this error to our support team via e-mail: <a href="mailto:tech.support@turris.cz">tech.support@turris.cz</a>.`,
+                            }}
+                        />
                     </div>
                 </div>
             );
